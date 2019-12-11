fdae02deae065b3b98ea87081b8148ebb6d46cb3
Abdelrahman-Arw <abdelrahman.arwish@linkdev.com>
2019-12-11 11:18:11 +0200

    second part

---
+++ RW - Paper/BookOpeningTransition.swift
@@ -0,0 +1,13 @@
+//
+//  BookOpeningTransition.swift
+//  RW - Paper
+//
+//  Created by Abdelrahman-Arw on 12/10/19.
+//  Copyright © 2019 -. All rights reserved.
+//
+
+import UIKit
+
+class BookOpeningTransition: NSObject {
+
+}
---
+++ RW - Paper/CustomNavigationControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomNavigationControllerViewController.swift
+//  RW - Paper
+//
+//  Created by Abdelrahman-Arw on 12/10/19.
+//  Copyright © 2019 -. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationControllerViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
